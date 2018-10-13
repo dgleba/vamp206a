f1cc6a877abbfa4ef083ff1f83e589973ff7da1c
David Gleba <dgleba@gmail.com>
2018-10-12 20:52:34 -0400

    2018-10-12d

---
+++ 100/303a-js-stack.sh
@@ -0,0 +1,78 @@
+#!/usr/bin/env bash
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+function Purpose() {
+# begin block comment =============================
+: <<'END'
+
+#  Purpose:  Install javascript stack(s)
+
+
+http://askubuntu.com/questions/594656/how-to-install-the-latest-versions-of-nodejs-and-npm-for-ubuntu-14-04-lts
+
+
+http://stackoverflow.com/questions/32426601/completly-uninstall-nodejs-npm-and-node-in-ubuntu-14-04
+I would recommend installing node using Node Version Manager(NVM). That saved a lot of head ache for me. Because you can install nodejs and npm without sudo using nvm.
+
+
+https://www.digitalocean.com/community/tutorials/how-to-install-node-js-on-an-ubuntu-14-04-server
+
+END
+# end block comment ===============================
+}
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+cd ; date ; set +vx  ; set -vx ; # echo off, then echo on
+#
+# Main: put code here...
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+sudo ls
+
+# remove it...
+ # npm uninstall -g sails 
+ # npm uninstall -g ember-cli 
+ # npm uninstall -g sane-cli 
+# remove...
+# sudo npm uninstall -g sails 
+# sudo npm uninstall -g ember-cli 
+# sudo npm uninstall -g syane-cli 
+sudo apt-get remove nodejs
+sudo apt-get remove npm
+sudo apt-get purge nodejs npm
+sudo apt-get update
+sudo apt-get autoremove
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+#install nvm
+# i didn't use nvm...
+sudo apt-get -y install build-essential libssl-dev
+#You should check the GitHub project page for the latest release of NVM, 
+#   https://github.com/creationix/nvm/releases
+#      edit v0.31.0 .....
+# curl https://raw.githubusercontent.com/creationix/nvm/v0.31.0/install.sh | bash
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash
+
+source ~/.profile
+source ~/.bashrc
+tail -n+10 ~/.bashrc |bash
+
+
+
+nvm --version
+#http://www.liquidweb.com/kb/how-to-install-nvm-node-version-manager-for-node-js-on-ubuntu-14-04-lts/
+
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+date
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+echo 
+echo 
+echo 
+echo 
+echo 'next run --  303b-js-stack.sh'
+echo 
+echo 
+echo 
---
+++ 100/303b-js-stack.sh
@@ -0,0 +1,79 @@
+#!/usr/bin/env bash
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+function Purpose() {
+# begin block comment =============================
+: <<'END'
+
+#  Purpose:  Install javascript stack(s)
+
+
+http://askubuntu.com/questions/594656/how-to-install-the-latest-versions-of-nodejs-and-npm-for-ubuntu-14-04-lts
+
+
+http://stackoverflow.com/questions/32426601/completly-uninstall-nodejs-npm-and-node-in-ubuntu-14-04
+I would recommend installing node using Node Version Manager(NVM). That saved a lot of head ache for me. Because you can install nodejs and npm without sudo using nvm.
+
+
+https://www.digitalocean.com/community/tutorials/how-to-install-node-js-on-an-ubuntu-14-04-server
+
+END
+# end block comment ===============================
+}
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+cd ; date ; set +vx  ; set -vx ; # echo off, then echo on
+#
+# Main: put code here...
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+sudo ls
+
+
+
+nvm --version
+#http://www.liquidweb.com/kb/how-to-install-nvm-node-version-manager-for-node-js-on-ubuntu-14-04-lts/
+
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+#worked...
+
+
+#http://sailsjs.org/get-started
+
+# sudo apt-get -y install python-software-properties python g++ make
+
+# curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
+
+
+
+
+
+#https://www.digitalocean.com/community/tutorials/how-to-install-node-js-on-ubuntu-16-04
+nvm install 8.9.0
+
+nvm use 8.9.0
+
+nvm ls
+
+
+
+
+node -v
+nvm --version
+npm -v
+sails -v
+
+npm install -g marked --save
+
+
+
+
+
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+date
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
