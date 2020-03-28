e94e5fe02e8b1d67a3ed02677467f2b218f69175
David Gleba <dgleba@gmail.com>
2020-03-27 21:11:58 -0400

    2020-03-27c

--- a7/21paste.sh
+++ a7/21paste.sh
@@ -120,6 +120,30 @@ echo .;
 sleep 9;
 # exit # logout to get a fresh env by loging back in.;
 
+
+# sudo chmod -R 775 $hpath/shc
+# sudo chmod -R 775 $hpath/bin2
+ chown -R $userv $hpath
+ chgrp -R $userv $hpath
+
+# copy scripts to home bin folder..
+mkdir -p bin
+  chown -R $userv bin
+  chgrp -R $userv bin
+cd
+cp shc/bin1/* bin
+  chmod -R +x bin
+
+# install github..rupa/z - z-jump
+  cd $HOME
+  mkdir -p $HOME/bin
+  # git clone https://github.com/dgleba/z.git  bin/zjump
+  git clone https://github.com/rupa/z.git  bin/zjump
+  echo  export _Z_MAX_SCORE=11000>>$HOME/.bashrc
+  echo . $HOME/bin/zjump/z.sh>>$HOME/.bashrc
+  source $HOME/.bashrc
+ 
+ 
 EOF
 
 export fil=provision01.sh ; export pth=~ ;  chmod +x $pth/$fil  ;  $pth/$fil   2>&1 | tee -a ${fil}_log$(date +"__%Y-%m-%d_%H.%M.%S").log;
